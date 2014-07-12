c8f13fe9be70ae074dce5cd7867670280796f799
Rabi Shanker Guha <guha.rabishankar@gmail.com>
2014-07-12 18:10:41 +0530

    Refactor Code and minor tweaks

--- test/src/file.py
+++ test/src/file.py
@@ -1,11 +1,20 @@
-from config import *
+import configparser
+import os
+
+BASEDIR = os.path.normpath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+_config = configparser.ConfigParser()
+_config.read(os.path.join(BASEDIR, 'config.ini'))
+config = _config['FILE']
+
 
 def save(key, value):
-    with open(KEYSTORE, "a") as keystore:
+    with open(config['KEYSTORE'], "a") as keystore:
         keystore.write("{} = {}\n".format(key, value))
 
+
 def fetch(key):
-    with open(KEYSTORE, "r") as keystore:
+    with open(config['KEYSTORE'], "r") as keystore:
         for line in keystore:
             if(line.startswith(key + " = ")):
                 val = line[line.index("=")+2:]
--- test/src/netifrc.py
+++ test/src/netifrc.py
@@ -1,9 +1,11 @@
+
 #!/usr/bin/python3
 
-import yaml
+import configparser
 import os
 import sys
 import subprocess
+from yaml import load as yamlLoad
 
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
@@ -11,43 +13,55 @@ except ImportError:
     from yaml import Loader, Dumper
 
 
-from config import *
-BASEDIR = os.path.normpath(os.path.join(os.path.dirname( __file__), os.pardir))
+BASEDIR = os.path.normpath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+config = configparser.ConfigParser()
+config.read(os.path.join(BASEDIR, 'config.ini'))
+defaults = config['GLOBALS']
 
 from file import save as backend_save, fetch as backend_retrieve
 
+
 def get_mode():
     if(os.path.exists("/var/run/openrc")):
-        return MODE_MASTER
-    return MODE_SLAVE
+        return defaults['MODE_MASTER']
+    return defaults['MODE_SLAVE']
+
 
 def normalize(*args):
     return '_'.join(''.join(c for c in arg if c.isalnum()) for arg in args)
 
+
 def init(data):
-    print("Backing up {}".format(CONFIG_FILE))
+    print("Backing up {}".format(defaults['CONFIG_FILE']))
     try:
-        if(os.path.isfile(CONFIG_FILE)):
-            subprocess.check_call(["mv", CONFIG_FILE, CONFIG_FILE_BACKUP])
+        if(os.path.isfile(defaults['CONFIG_FILE'])):
+            subprocess.check_call(["mv", defaults['CONFIG_FILE'],
+                                   defaults['CONFIG_FILE_BACKUP']])
+
     except subprocess.CalledProcessError:
         print("Could not backup Config File")
         sys.exit(1)
 
     with open(BASEDIR + "/" + data['net_config'], 'r') as current_config_file:
         current_config = current_config_file.read()
-    with open(CONFIG_FILE, 'w') as config_file:
+    with open(defaults['CONFIG_FILE'], 'w') as config_file:
         config_file.write(current_config)
 
     try:
-        subprocess.check_call(["rc-service", "net."+data['interface'], "restart"])
+        subprocess.check_call(["rc-service", "net."+data['interface'],
+                               "restart"])
     except subprocess.CalledProcessError:
         print("Could not effectively start process net."+data['interface'])
         sys.exit(1)
 
+
 def finalize(data):
-    print("Restoring {}".format(CONFIG_FILE))
+    print("Restoring {}".format(defaults['CONFIG_FILE']))
     try:
-        subprocess.check_call(["mv", CONFIG_FILE_BACKUP, CONFIG_FILE])
+        subprocess.check_call(["mv",
+                               defaults['CONFIG_FILE_BACKUP'],
+                               defaults['CONFIG_FILE']])
     except subprocess.CalledProcessError:
         print("Could not Restore Config File")
         sys.exit(1)
@@ -58,12 +72,14 @@ def finalize(data):
         print("Could not effectively stop process net." + data['interface'])
         sys.exit(1)
 
+
 def test(data, mode):
     for test in data['tests']:
         print(test['name'])
-        command = subprocess.Popen(test['command'], stdout=subprocess.PIPE, shell=True)
+        command = subprocess.Popen(test['command'], stdout=subprocess.PIPE,
+                                   shell=True)
         try:
-            (out, err) = command.communicate(timeout=TIMEOUT)
+            (out, err) = command.communicate(timeout=int(defaults['TIMEOUT']))
         except subprocess.TimeoutExpired:
             print("Command {} Expired".format(test['command']))
             command.kill()
@@ -71,36 +87,57 @@ def test(data, mode):
         else:
             for key in test['keys']:
                 key_command = subprocess.Popen(key['value'],
-                        stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+                                               stdin=subprocess.PIPE,
+                                               stdout=subprocess.PIPE,
+                                               shell=True)
                 try:
-                    (stdout, stderr) = key_command.communicate(input=out, timeout=TIMEOUT)
+                    (stdout, stderr) = key_command.communicate(
+                        input=out,
+                        timeout=int(defaults['TIMEOUT']))
+
                 except subprocess.TimeoutExpired:
-                    print("Trying to find the value of {} timed out".format(key['name']))
+                    print("Trying to find the value of {} timed out"
+                          .format(key['name']))
                     key_command.kill()
-                    key_process.communicate()
+                    key_command.communicate()
+
                 else:
                     if('type' in key and key['type'] == "boolean"):
                         value = str(key_command.returncode)
                     else:
                         value = stdout.decode("utf-8").strip()
-                    #print("{} is {}".format(key['name'],value))
-                    if(mode == MODE_MASTER):
-                        backend_save(normalize(data['name'], test['name'], key['name']), value)
+
+                    if(mode == defaults['MODE_MASTER']):
+                        backend_save(normalize(data['name'],
+                                               test['name'],
+                                               key['name']), value)
                     else:
-                        backend_value = backend_retrieve(normalize(data['name'], test['name'], key['name']))
+                        backend_value = backend_retrieve(
+                            normalize(data['name'], test['name'], key['name']))
                         print("{} {}".format(value, backend_value))
                         assert value == backend_value
+
 
 for file in sys.argv[1:]:
     with open(file, 'r') as f:
         document = f.read()
-    data = yaml.load(document, Loader=Loader)
+
+    # Replace with the variables defined in SPECS
+    for var in config['SPECS']:
+        document = document.replace("$${}$$".format(var.upper()),
+                                    config['SPECS'][var])
+
+    # Parse the yaml file
+    data = yamlLoad(document, Loader=Loader)
+
     try:
         mode = os.environ['MODE']
     except KeyError:
         mode = ""
-    if(mode != MODE_MASTER and mode != MODE_SLAVE):
+
+    if(mode != defaults['MODE_MASTER'] and mode != defaults['MODE_SLAVE']):
         mode = get_mode()
+
     try:
         init(data)
         test(data, mode=mode)
