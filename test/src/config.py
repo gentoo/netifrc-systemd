5580fa1085b2f7d705340aa59302ef096747f8e6
Rabi Shanker Guha <guha.rabishankar@gmail.com>
2014-07-12 18:09:04 +0530

    Move config to config.ini

--- test/src/config.py
+++
@@ -1,8 +0,0 @@
-MODE_MASTER = "master"
-MODE_SLAVE = "slave"
-CONFIG_FILE = "/etc/conf.d/net"
-CONFIG_FILE_BACKUP = CONFIG_FILE+".backup"
-TIMEOUT=5
-
-# Configuration for file backend
-KEYSTORE = "/tmp/netifrc_test.py"
